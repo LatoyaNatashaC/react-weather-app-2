325ecb30f8bd088da627ab0a840ba5bed1ecbcbf
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-10-01 12:57:12 -0700

    link to GitHub url added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function App() {
-  return <div className="App">Weather App #2</div>;
+  return (
+    <div className="App">
+      <h1>React Weather App #2</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/LatoyaNatashaC?tab=repositories"
+          target="_blank"
+        >
+          Latoya Natasha Campbell
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/LatoyaNatashaC?tab=repositories"
+          target="_blank"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
