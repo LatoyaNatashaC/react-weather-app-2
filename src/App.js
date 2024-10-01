78f15d181cb63b4cf2ecfabd59ac787d913c0c83
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-10-01 14:10:32 -0700

    bootstrap imported

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App #2</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/LatoyaNatashaC?tab=repositories"
-          target="_blank"
-        >
-          Latoya Natasha Campbell
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/LatoyaNatashaC?tab=repositories"
-          target="_blank"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>React Weather App #2</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/LatoyaNatashaC?tab=repositories"
+            target="_blank"
+          >
+            Latoya Natasha Campbell
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/LatoyaNatashaC?tab=repositories"
+            target="_blank"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
