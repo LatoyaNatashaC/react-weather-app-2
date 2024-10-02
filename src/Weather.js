41f3d42c2ce3f7831958a8b49012f0feec274488
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-10-02 13:01:55 -0700

    form created

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,21 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+
       <h1>Los Angeles</h1>
       <ul>
         <li>Wednesday 9:00 Am</li>
