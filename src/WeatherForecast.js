c965ad6ea546f841c038f9f60016ff09171aff10
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-10-09 11:16:17 -0700

    forecast frame created

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,20 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
-export default function WeatherForecast() {
-  return "forecast will be here";
+export default function WeatherForecast(props) {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thur</div>
+          <WeatherIcon code="clear-sky-day" size={36} />
+          <div className="WeatherForecast-temperature">
+            <span className="WeatherForecast-temperature-max">84º</span>
+            <span className="WeatherForecast-temperature-min"> 56º</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
