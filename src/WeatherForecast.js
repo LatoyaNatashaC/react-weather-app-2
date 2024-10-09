ac1d3195fce8b8859c21e64a8e95b1750abfb0a8
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-10-09 11:24:31 -0700

    axios imported

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast(props) {
   let apiKey = "4f3b0tf3219b4c7758082d0o48eabbbe";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(handleForecastResponse);
 
   return (
     <div className="WeatherForecast">
